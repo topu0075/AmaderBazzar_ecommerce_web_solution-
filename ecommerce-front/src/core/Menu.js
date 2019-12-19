d49904612eb50a88827bfeebfc80cce6b69cf9ce
Topu <rahman.topu67@gmail.com>
2019-12-19 20:56:36 +0600

    first commit

---
+++ ecommerce-front/src/core/Menu.js
@@ -0,0 +1,121 @@
+import React, { Fragment } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { signout, isAuthenticated } from "../auth";
+import { itemTotal } from "./cartHelpers";
+
+const isActive = (history, path) => {
+    if (history.location.pathname === path) {
+        return { color: "#000000" ,
+                  background: "#ffffff"
+        
+        };
+    } else {
+        return { color: "#ffffff" };
+    }
+};
+
+const Menu = ({ history }) => (
+    <div>
+        <ul className="nav nav-tabs bg-dark ">
+            <li className="nav-item" >
+                <Link
+                    className="nav-link"
+                    style={isActive(history, "/")}
+                    to="/"
+                >
+                    AmaderBazar
+                </Link>
+            </li>
+
+            <li className="nav-item mr-auto">
+                <Link
+                    className="nav-link"
+                    style={isActive(history, "/shop")}
+                    to="/shop"
+                >
+                    Products
+                </Link>
+            </li>
+
+            {isAuthenticated() && isAuthenticated().user.role === 0 && (
+                <li className="nav-item">
+                    <Link
+                        className="nav-link"
+                        style={isActive(history, "/user/dashboard")}
+                        to="/user/dashboard"
+                    >
+                        Dashboard
+                    </Link>
+                </li>
+            )}
+
+            {isAuthenticated() && isAuthenticated().user.role === 1 && (
+                <li className="nav-item ml-auto">
+                    <Link
+                        className="nav-link"
+                        style={isActive(history, "/admin/dashboard")}
+                        to="/admin/dashboard"
+                    >
+                        Dashboard
+                    </Link>
+                </li>
+            )}
+
+            {!isAuthenticated() && (
+                <Fragment>
+                    <li className="nav-item">
+                        <Link
+                            className="nav-link"
+                            style={isActive(history, "/signin")}
+                            to="/signin"
+                        >
+                            Signin
+                        </Link>
+                    </li>
+
+                    <li className="nav-item">
+                        <Link
+                            className="nav-link"
+                            style={isActive(history, "/signup")}
+                            to="/signup"
+                        >
+                            Signup
+                        </Link>
+                    </li>
+                </Fragment>
+            )}
+
+            {isAuthenticated() && (
+                <li className="nav-item ml-auto">
+                    <span
+                        className="nav-link"
+                        style={{ cursor: "pointer", color: "#ffffff" }}
+                        onClick={() =>
+                            signout(() => {
+                                history.push("/");
+                            })
+                        }
+                    >
+                        Signout
+                    </span>
+                </li>
+            )}
+
+            <li className="nav-item">
+                <Link
+                    className="nav-link"
+                    style={isActive(history, "/cart")}
+                    to="/cart"
+                >
+                    Cart{" "}
+                    <sup>
+                        <small className="cart-badge">{itemTotal()}</small>
+                    </sup>
+                </Link>
+            </li>
+
+        </ul>
+    </div>
+);
+
+export default withRouter(Menu);
---
+++ ecommerce-front/src/core/fixedPrices.js
@@ -0,0 +1,32 @@
+export const prices = [
+    {
+        _id: 0,
+        name: "Any",
+        array: []
+    },
+    {
+        _id: 1,
+        name: "0 to 2000 taka",
+        array: [0, 1999]
+    },
+    {
+        _id: 2,
+        name: "2000 to 10000 taka",
+        array: [2000, 9999]
+    },
+    {
+        _id: 3,
+        name: "10000 to 50000 taka",
+        array: [10000, 49999]
+    },
+    {
+        _id: 4,
+        name: "50000 to 150000 taka",
+        array: [50000, 149999]
+    },
+    {
+        _id: 5,
+        name: "More than 150000 taka",
+        array: [150000, 999999]
+    }
+];
